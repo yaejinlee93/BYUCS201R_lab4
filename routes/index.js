b6490c72c6b23e88b224db3acebdd8d3dbb31fb1
yaejinlee93 <yaejinlee93@gmail.com>
2017-03-07 22:34:52 -0700

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('weather.html', { root:  'public' });
+});
+
+router.get('/getcity',function(req,res,next) {
+	console.log("In getcity route");
+
+	var fs = require('fs');
+
+	fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
+  		if(err) throw err;
+		var cities = data.toString().split("\n");
+		for(var i = 0; i < cities.length; i++) {
+			var result = cities[i].search(myRe);
+			if(result != -1) {
+			console.log(cities[i]);
+			}
+ 		}
+		var jsonresult = [];
+		for(var i = 0; i < cities.length; i++) {
+		var result = cities[i].search(myRe);
+		if(result != -1) {
+			console.log(cities[i]);
+			jsonresult.push({city:cities[i]});
+		}
+}
+//console.log(jsonresult);
+res.status(200).json(jsonresult);
+	});
+
+	var myRe = new RegExp("^" + req.query.q);
+	console.log(myRe);
+
+});
+
+router.get('/word',function(req,res)
+{
+  var url = "https://owlbot.info/api/v1/dictionary/" + req.query.word + "?format=json";
+  https.get(url, function(owl){
+    owl.pipe(res);
+  });
 });
 
 module.exports = router;
